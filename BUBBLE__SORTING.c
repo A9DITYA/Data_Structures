d5a321e6744cfb6f6e8867854086ab3e4ef182e7
Aditya Ranjan <116148494+A9DITYA@users.noreply.github.com>
2023-04-23 21:35:51 +0530

    Update BUBBLE__SORTING.c

--- BUBBLE__SORTING.c
+++ BUBBLE__SORTING.c
@@ -1,46 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-
-void swap(int *x, int *y)//the swap function//
+void swap(int *x,int *y)
 {
-    int temp=*x;
-    *x=*y;
-    *y=temp;
+ int temp=*x;
+ *x=*y;
+ *y=temp;
 }
-
-
-void Bubble(int A[],int n)// function for the bubble//
+void Bubble(int A[],int n)
 {
-    int i,j,flag=0;;
+ int i,j,flag=0;
 
-    for(i=0;i<n-1;i++)// it proceeds for the passes i,e (n-1) passes//
-    {
-        for(j=0;j<n-i-1;j++) //it proceeds for the elements in the passes // 
-        {
-            if(A[j]>A[j+1]) //compare the element of column and proceed for the next pass//
-            {
-            swap(&A[j],&A[j+1]);//if the condition is true then swap --- so flag==1//
-            flag==1;
-            }
+ for(i=0;i<n-1;i++)
+ {
+ flag=0;
+ for(j=0;j<n-i-1;j++)
+ {
+ if(A[j]>A[j+1])
+ {
+ swap(&A[j],&A[j+1]);
+ flag=1;
+ }
+ }
+ if(flag==0)
+ break;
+ }
 
-        }if(flag==0) 
-        break;
-    }
-    
 }
-
 int main()
 {
-    int A[]={7,9,17,8,44,12,4,11,2,3}, n=10,i;
-    Bubble(A,n);
+ int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
 
-    for(i=0;i<10;i++)
-    {
-        printf("%d", A[i]);
-        printf("\n");
-    }
-    
+ Bubble(A,n);
 
-    return 0;
-  
+ for(i=0;i<10;i++)
+ printf("%d ",A[i]);
+ printf("\n");
+
+ return 0;
 }
